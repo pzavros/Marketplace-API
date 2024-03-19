56bd1bc49fd7bbfafb60d86d744a9b11a68530d4
Panayiotis Zavros <panazavros8@gmail.com>
2024-03-19 11:52:12 +0200

    final

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const sequelize = new Sequelize({
 
 
 
+
 // Define Models
 // Create Category table
 const Category = sequelize.define('Category', {
@@ -23,7 +24,7 @@ const Category = sequelize.define('Category', {
     allowNull: false,
     unique: true,
     validate: {
-      len: [0, 50] // Ensures the name is up to 50 characters long
+      len: [0, 50]
     }
   }
 }, {
@@ -62,11 +63,10 @@ const User = sequelize.define('User', {
     type: DataTypes.NUMBER,
     allowNull: false,
     defaultValue: 0
-  }
+  },
 }, {
   timestamps: false
 });
-
 
 // Define Cart model
 const Cart = sequelize.define('Cart', {
@@ -83,19 +83,83 @@ const Cart = sequelize.define('Cart', {
       model: User,
       key: 'id'
     }
-  },
-  // Define an array to store product IDs
-  productIds: {
-    type: DataTypes.ARRAY(DataTypes.INTEGER),
-    defaultValue: []
   }
 }, {
   timestamps: false
 });
 
-// Define association between User and Cart
-User.hasOne(Cart, { foreignKey: 'userId' });
-Cart.belongsTo(User, { foreignKey: 'userId' });
+
+const CartProduct = sequelize.define('CartProduct', {
+  cartId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Cart',
+      key: 'id'
+    }
+  },
+  productId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Product',
+      key: 'id'
+    }
+  }
+}, {
+  timestamps: false
+});
+
+
+// define Order model
+const Order = sequelize.define('Order', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'id'
+    }
+  },
+  createdOn: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+}, {
+  timestamps: false 
+});
+
+// define OrderProduct model
+const OrderProduct = sequelize.define('OrderProduct', {
+  orderId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Orders',
+      key: 'id'
+    }
+  },
+  productId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Products',
+      key: 'id'
+    }
+  }
+}, {
+  timestamps: false
+});
+
+
+// associations 
+Order.belongsToMany(Product, { through: OrderProduct, foreignKey: 'orderId' });
+Product.belongsToMany(Order, { through: OrderProduct, foreignKey: 'productId' });
+
+Cart.belongsToMany(Product, { through: CartProduct, foreignKey: 'cartId' });
+Product.belongsToMany(Cart, { through: CartProduct, foreignKey: 'productId' });
+
 
 
 // Initialize the express application
@@ -106,28 +170,11 @@ const port = 3000;
 app.use(express.json());
 
 
+// ------------All routes-----------------
 
 
-// Category Routes
-app.get('/api/categories', async (req, res) => {
-  try {
-    const categories = await Category.findAll();
-    res.status(200).json(categories);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-app.get('/api/:id', async (req, res) => {
-  try {
-    const category = await Category.findByPk(req.params.id);
-    res.status(200).json(category);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-//Create Category
+// -----------Category Routes----------------
+// create category
 app.post('/api/categories/create', async (req, res) => {
   const { name } = req.body;
   if (!name || name.length > 50) {
@@ -149,7 +196,7 @@ app.post('/api/categories/create', async (req, res) => {
 app.delete('/api/categories/delete/:id', async (req, res) => {
   const id = req.params.id;
 
-  // Check if the ID is valid and a positive integer
+  // Check if the ID is valid
   if (!id || isNaN(id) || parseInt(id) < 1) {
     return res.status(422).json({ error: 'Invalid ID provided.' });
   }
@@ -169,12 +216,13 @@ app.delete('/api/categories/delete/:id', async (req, res) => {
     res.status(422).json({ error: error.message });
   }
 });
-// End of Category Routes
+// ---------------------End of Category Routes-----------------------
 
 
 
 
-// Product Routes
+// ---------------------Product Routes-----------------------------
+// Create Product
 app.post('/api/product/create', async (req, res) => {
   const { name, description = "No product description", price, stock, categoryId } = req.body;
 
@@ -204,10 +252,40 @@ app.post('/api/product/create', async (req, res) => {
 
 });
 
+// List products
+app.get('/api/products/list', async (req, res) => {
+  try {
+    if (req.query.categoryId) {
+      const categoryId = parseInt(req.query.categoryId);
+      if (isNaN(categoryId) || categoryId <= 0) {
+        return res.status(422).json({ error: 'Invalid category ID' });
+      }
+
+      const products = await Product.findAll({
+        where: {
+          categoryId
+        }
+      });
+
+      return res.status(200).json(products);
+    }
+
+
+
+    const products = await Product.findAll();
+
+    return res.status(200).json(products);
+  } catch (error) {
+    console.error('Error details:', error);
+    return res.status(500).json({ error: error.message || 'Error listing the products' });
+  }
+});
+
+// Get Product by id
 app.get('/api/product/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    // Check if id is a valid integer
+    // Check if id is a valid
     if (isNaN(id) || parseInt(id) <= 0) {
       return res.status(422).json({ error: 'Invalid ID' });
     }
@@ -224,6 +302,7 @@ app.get('/api/product/:id', async (req, res) => {
   }
 });
 
+// Delete Product
 app.delete('/api/product/delete/:id', async (req, res) => {
   const { id } = req.params;
   try {
@@ -242,19 +321,7 @@ app.delete('/api/product/delete/:id', async (req, res) => {
 
 });
 
-app.get('/api/products/list', async (req, res) => {
-  try {
-    const products = await Product.findAll({
-      include: Category
-    });
-
-    return res.status(200).json(products);
-  } catch (error) {
-    console.error('Error details:', error);
-    return res.status(500).json({ error: error.message || 'Error listing the products' });
-  }
-});
-
+// Update product
 app.put('/api/product/update/:id', async (req, res) => {
   const { id } = req.params;
   const { name, description, price, stock, categoryId } = req.body;
@@ -263,6 +330,10 @@ app.put('/api/product/update/:id', async (req, res) => {
     const product = await Product.findByPk(id);
     if (!product) {
       return res.status(404).json({ error: 'Product not found' });
+    }
+
+    if(id <= 0 || isNaN(id)) {
+      return res.status(422).json({ error: 'Invalid ID' });
     }
 
     if (name) {
@@ -280,27 +351,32 @@ app.put('/api/product/update/:id', async (req, res) => {
     if (categoryId) {
       product.categoryId = categoryId;
     }
+    
+    // Save the updates to the database
+    await product.save();
 
-
-    return res.status(200).json({ message: 'Product deleted successfully' });
+   
+    return res.status(200).json({ message: 'Product updated successfully' });
   } catch (error) {
-    return res.status(500).json({ error: error.message || 'Error updating the product' });
+    return res.status(500).json({ 
+      error: error.message || 'Error updating the product' 
+    });
   }
 });
-// End of Product Routes
+// -------------------End of Product Routes-----------------------
 
 
 
 
-// User Routes
+// --------------------------User Routes-----------------------
 // Create User
 app.post('/api/user/create', async (req, res) => {
-  const { username, accountBalance} = req.body;
+  const { username, accountBalance } = req.body;
 
   if (!username) {
     return res.status(422).json({ error: 'Invalid or missing parameters' });
   }
-  
+
   try {
     const user = await User.create({
       username,
@@ -309,10 +385,39 @@ app.post('/api/user/create', async (req, res) => {
 
     return res.status(200).json(user);
   } catch (error) {
-    console.error('Error details:', error);
     return res.status(500).json({ error: error.message || 'Error creating the user' });
   }
 
+});
+
+app.get('/api/user/getCart', async (req, res) => {
+  const userId = parseInt(req.query.userId);
+  if (!userId) {
+    return res.status(422).json({ error: 'User ID is required.' });
+  }
+
+  try {
+    // Find or create a cart for the user
+    const [cart, created] = await Cart.findOrCreate({
+      where: { userId },
+      include: [{
+        model: Product,
+        as: 'Products',
+      }],
+    });
+
+    const productIDs = cart.Products.map(product => product.id);
+
+    return res.status(200).json({
+      cartID: cart.id,
+      userID: cart.userId,
+      productIDs
+    });
+
+
+  } catch (error) {
+    return res.status(422).json({ error: 'Failed to retrieve cart items.' });
+  }
 });
 
 // List Users
@@ -321,8 +426,7 @@ app.get('/api/user/list', async (req, res) => {
     const users = await User.findAll();
     res.status(200).json(users);
   } catch (error) {
-    console.error('Error', error);
-    res.status(422).json({ error: 'data items could not be retrieved. ' });
+    res.status(422).json({ error: 'Failed to retreive data' });
   }
 });
 
@@ -350,140 +454,255 @@ app.delete('/api/user/delete/:id', async (req, res) => {
     if (isNaN(id) || id <= 0 || id === '') {
       return res.status(422).json({ error: 'Invalid ID' });
     }
-    // const orders = await Order.findAll({
-    //   where: {
-    //     userId: id
-    //   }
-    // });
-    // const carts = await Cart.findOne({
-    //   where: {
-    //     userId: id
-    //   }
-    // });
+    const orders = await Order.findAll({
+      where: {
+        userId: id
+      }
+    });
+    const carts = await Cart.findOne({
+      where: {
+        userId: id
+      }
+    });
 
-    // if (orders) {
-    //   return res.status(422).json({
-    //     error: 'User is referenced on Orders table'
-    //   });
-    // }
-    // if (carts) {
-    //   return res.status(422).json({
-    //     error: 'User is referenced on Carts table'
-    //   });
-    // }
+    const users = await User.findByPk(id);
+
+    if(users){
+      if (orders && users.orderId) {
+      return res.status(422).json({
+        error: 'User is referenced on Orders table'
+      });
+    }
+    }
+    else{
+      return res.status(404).send('User not found');
+    }
+    
+    if (carts) {
+      return res.status(422).json({
+        error: 'User is referenced on Carts table'
+      });
+    }
 
     const user = await User.destroy({
       where: {
         id
       }
     });
-    if (user === 0) { // No user was deleted
-      return res.status(404).send('User not found');
-    }
-    // Include logic here to also delete the user's cart if required
+
     return res.status(200).send('User Deleted successfully');
   } catch (error) {
     return res.status(500).json({ error: error.message || 'Error deleting the user' });
   }
 });
 
-// Get user's cart
-// app.get('/api/user/:userId/getCart', async (req, res) => {
-//   const { userId } = req.params;
+// Add to cart
+app.post('/api/user/addToCart', async (req, res) => {
+  const { userID, productID } = req.body;
 
-//   try {
-//     // Validate user ID
-//     if (isNaN(userId) || parseInt(userId) <= 0) {
-//       return res.status(422).json({ error: 'Invalid user ID' });
-//     }
+  try {
+    // User does not exists
+    const user = await User.findByPk(userID);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
 
-//     const user = await User.findByPk(userId);
+    // Product does not exists
+    const product = await Product.findByPk(productID);
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
 
-//     // Check if user exists
-//     if (!user) {
-//       return res.status(404).json({ error: 'User not found' });
-//     }
+    // Find or create a cart for the user
+    let cart = await Cart.findOne({ where: { userId: userID } });
+    if (!cart) {
+      cart = await Cart.create({ userId: userID });
+    }
 
-//     // Fetch the user's cart
-//     const cart = await user.getCart();
+    // product is already added in the cart
+    const existingCartItem = await CartProduct.findOne({ where: { cartId: cart.id, productId: productID } });
+    if (existingCartItem) {
+      return res.status(422).json({ error: 'Product already in cart' });
+    }
 
-//     // Check if cart exists
-//     if (!cart) {
-//       return res.status(422).json({ error: 'Cart not found' });
-//     }
+    
+    await CartProduct.create({ cartId: cart.id, productId: productID });
 
-//     // Fetch product details for each product ID in the cart
-//     const products = await Promise.all(
-//       cart.productIds.map(async (productId) => await Product.findByPk(productId))
-//     );
+    return res.status(200).json({ message: 'Product added to cart successfully' });
 
-//     // Return response with cart details and product information
-//     return res.status(200).json({
-//       cartId: cart.id,
-//       userId: cart.userId,
-//       productIds: products.map((product) => product.id)
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ error: 'Error retrieving cart' });
-//   }
-// });
+  } catch (error) {
+    console.error('Error details:', error);
+    return res.status(500).json({ error: error.message || 'Error adding product to cart' });
+  }
+});
+
+// Remove from cart
+app.delete('/api/user/removeFromCart', async (req, res) => {
+  const { userID, productID } = req.body;
+
+  // Validate request body
+  if (!userID || !productID) {
+    return res.status(422).json({ error: 'User ID and Product ID are required.' });
+  }
+
+  try {
+    // userr does not exists
+    const userExists = await User.findByPk(userID);
+    if (!userExists) {
+      return res.status(422).json({ 
+        error: 'User does not exist.' 
+      });
+    }
+
+    // product does not exists
+    const productExists = await Product.findByPk(productID);
+    if (!productExists) {
+      return res.status(422).json({ 
+        error: 'Product does not exist.' 
+      });
+    }
+
+    // Find the user's cart
+    const cart = await Cart.findOne({ 
+      where: { userId: userID } 
+    });
+    if (!cart) {
+      return res.status(404).json({ 
+        error: 'Cart not found for the user.' 
+      });
+    }
+
+    // remove product
+    const removedItem = await CartProduct.destroy({
+      where: {
+        cartId: cart.id,
+        productId: productID
+      }
+    });
 
 
-// // Add product to user's cart
-// app.post('/api/user/:userId/addToCart', async (req, res) => {
-//   const { userId, productId } = req.body;
 
-//   try {
-//     // Validate user ID and product ID
-//     if (isNaN(userId) || parseInt(userId) <= 0 || isNaN(productId) || parseInt(productId) <= 0) {
-//       return res.status(422).json({ error: 'Invalid user ID or product ID' });
-//     }
+    // Return the updated cart
+    return res.status(200).json({ removedItem });
+  } catch (error) {
+    return res.status(422).json({ 
+      error: 'Failed to remove product from cart.',
+    });
+  }
+  
+});
 
-//     const user = await User.findByPk(userId);
+// Purchase
+app.post('/api/user/purchase', async (req, res) => {
+  const { userID } = req.body;
 
-//     // Check if user exists
-//     if (!user) {
-//       return res.status(404).json({ error: 'User not found' });
-//     }
+  
+  if (!userID) {
+    return res.status(422).json({ error: 'User ID is required.' });
+  }
 
-//     const product = await Product.findByPk(productId);
+  try {
+    
+    const user = await User.findByPk(userID);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
 
-//     // Check if product exists
-//     if (!product) {
-//       return res.status(404).json({ error: 'Product not found' });
-//     }
+    // Get user's cart
+    const cart = await Cart.findOne({
+      where: { userId: userID },
+      include: [{ model: Product}]
+    });
 
-//     // Fetch the user's cart
-//     const cart = await user.getCart();
+    if (!cart) {
+      return res.status(422).json({ error: 'The cart is empty.' });
+    }
 
-//     // Check if cart exists
-//     if (!cart) {
-//       // Create a new cart if it doesn't exist
-//       const newCart = await Cart.create({ userId });
-//       cart = newCart;
-//     }
+    // calculate total price
+    let totalPrice = 0;
+    let price = cart.Products.map(p => p.price);
+    for (let i = 0; i < price.length; i++) {
+      totalPrice += price[i];
+    }
 
-//     // Check if product already exists in the cart
-//     if (cart.productIds.includes(productId)) {
-//       return res.status(422).json({ error: 'Product already exists in the cart' });
-//     }
+    if (user.accountBalance < totalPrice) {
+      return res.status(422).json({ error: 'Insufficient account balance.' });
+    }
 
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ error: 'Error adding product to cart' });
-//   }
-// }
-// );
+    // Remove the price of the user's balance
+    await User.update({ 
+      accountBalance: user.accountBalance - totalPrice 
+    }, { 
+      where: { id: userID } 
+    });
 
+    // Create a new Order and OrderProduct entries
+    const order = await Order.create({ 
+      userId: userID, 
+      createdOn: Math.floor(Date.now() / 1000) 
+    });
+   
+    for (let i=0; i < cart.Products; i++) {
+      await OrderProduct.create({ 
+        orderId: order.id, 
+        productId: product.id 
+      });
+    }
+
+
+    return res.status(200).json({ message: 'Purchase successful', productIDs: cart.Products.map(p => p.id) });
+  } catch (error) {
+    console.error('Purchase Error:', error);
+    return res.status(422).json({ error: 'Failed to complete purchase.' });
+  }
+});
+
+// List Orders
+app.get('/api/order/list', async (req, res) => {
+  try {
+    const { userID, productID } = req.query;
+
+    let options = {
+      include: [{
+        model: Product,
+      }]
+    };
+
+    if (productID) {
+      options.include[0].where.id = productID;
+    }
+
+    if (userID) {
+      options.where.userId = userID;
+    }
+
+    const orders = await Order.findAll(options);
+
+    const response = orders.map(order => ({
+      id: order.id,
+      userID: order.userId,
+      createdOn: order.createdOn,
+      products: order.Products.map(product => product.id)
+    }));
+
+    return res.status(200).json({ orders: response });
+  } catch (error) {
+    console.error('List Orders Error:', error);
+    return res.status(422).json({ error: 'Failed to list orders.' });
+  }
+});
+// ---------------End of user routes--------------------
 
 
 
 
 
 // start API server
-
-app.listen(port, () => {
-  console.log(`server listening at port 3000`)
-  console.log(`Press Ctrl+C to exit...`)
+sequelize.sync({ force: false }).then(() => {
+  app.listen(port, () => {
+    console.log(`API server running at http://localhost:${port}`);
+  });
+}).catch(error => {
+  console.error('Unable to sync database:', error);
 });
+
